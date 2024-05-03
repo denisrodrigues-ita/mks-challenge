bce8d201ef837564971723e014523014408bcf11
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-05-03 09:32:59 -0300

    test: button test

---
+++ __tests__/components/atoms/button/button.test.tsx
@@ -0,0 +1,19 @@
+// Button.test.js
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { Button } from "@/components/atoms";
+
+describe("Button", () => {
+  it("should render the button with the text", () => {
+    const buttonText = "Click me";
+    render(<Button>{buttonText}</Button>);
+    expect(screen.getByText(buttonText)).toBeInTheDocument();
+  });
+
+  it("should call the onClick function when clicked", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Click me</Button>);
+    fireEvent.click(screen.getByText("Click me"));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
